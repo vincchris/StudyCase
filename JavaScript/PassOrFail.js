4aa17bb3439ad494b9cbff1b3ef64dd876c4a59b
Vincent Chriss <vchristianthen@gmail.com>
2026-07-19 20:20:40 +0700

    Update Progress Challenge 03

---
+++ JavaScript/PassOrFail.js
@@ -0,0 +1,25 @@
+/*
+Challenge 03: Pass or Fail
+
+Determine whether a student passes or fails based on the score.
+
+Rules:
+- Score >= 75 → Pass
+- Otherwise → Fail
+
+Example:
+Score = 80
+
+Output:
+Pass
+*/
+
+let score = 80
+
+if (score >= 75) {
+  console.log("Pass")
+}
+
+else {
+  console.log("Fail")
+}
