02e0d5bd4a14f49ada453644c126cda027c9b7b4
Vincent Chriss <vchristianthen@gmail.com>
2026-07-20 21:08:39 +0700

    Update Progress Challenge 04

---
+++ JavaScript/OddAndEven.js
@@ -0,0 +1,19 @@
+/*
+Challenge 04: Odd or Even
+
+Determine whether a number is odd or even.
+
+Example:
+Input: 19
+
+Output:
+Odd
+*/
+
+let number = 20
+
+if (number %= 2) {
+  console.log("Odd")
+} else {
+  console.log("Even")
+}
---
+++ Python/OddAndEven.py
@@ -0,0 +1,16 @@
+# Challenge 04: Odd or Even
+
+# Determine whether a number is odd or even.
+
+# Example:
+# Input: 19
+
+# Output:
+# Odd
+
+user_input = int(input("Input a Number : "))
+
+if user_input % 2:
+  print("Odd")
+else:
+  print("Even")
