e610705fd68fc75a485d1f70b7733a397748f2a0
Vincent Chriss <vchristianthen@gmail.com>
2026-07-22 20:54:12 +0700

    Update Challenge 06 JS dan Python

---
+++ JavaScript/CountToTen.js
@@ -0,0 +1,18 @@
+/*
+Challenge 06: Count to Ten
+
+Print all numbers from 1 to 10.
+
+Output:
+1
+2
+3
+...
+10
+*/
+
+let Number = 11
+
+for (let i = 0; i < Number; i++) {
+  console.log(i)
+}
---
+++ Python/CountToTen.py
@@ -0,0 +1,15 @@
+# Challenge 06: Count to Ten
+
+# Print all numbers from 1 to 10.
+
+# Output:
+# 1
+# 2
+# 3
+# ...
+# 10
+
+number = int(input("Input a Number : "))
+
+for i in range(number):
+  print(i)
