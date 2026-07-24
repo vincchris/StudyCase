1392d6698a6f5e23f061fae4f6aceb4bf5adf6fb
Vincent Chriss <vchristianthen@gmail.com>
2026-07-24 16:49:33 +0700

    Update Challenge 08 Python

---
+++ Python/MultiplicationTable.py
@@ -0,0 +1,19 @@
+# Challenge 08: Multiplication Table
+
+# Print the multiplication table for a given number.
+
+# Example:
+# Input: 5
+
+# Output:
+# 5 x 1 = 5
+# ...
+# 5 x 10 = 50
+
+total = 0
+number = int(input("Input a Number : "))
+
+for x in range(1, number + 1):
+  if x <= number:
+    total = number * x
+    print(total)
