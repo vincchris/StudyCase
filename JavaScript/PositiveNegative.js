6c880632c5bf9801eed17ae476b06b98fc9535bb
Vincent Chriss <vchristianthen@gmail.com>
2026-07-27 20:45:08 +0700

    Update Challenge 11

---
+++ JavaScript/PositiveNegative.js
@@ -0,0 +1,20 @@
+/*
+Challenge 11: Positive or Negative
+
+Determine whether a given number is positive,
+negative, or zero.
+
+Example:
+Input: -5
+
+Output:
+Negative
+*/
+
+let number = -5
+
+if (number < 0) {
+  console.log("Negative")
+} else {
+  console.log("Positive")
+}
---
+++ Python/PositiveNegative.py
@@ -0,0 +1,17 @@
+# Challenge 11: Positive or Negative
+
+# Determine whether a given number is positive,
+# negative, or zero.
+
+# Example:
+# Input: -5
+
+# Output:
+# Negative
+
+number = int(input("Input a Number : "))
+
+if number < 0:
+  print("Negative")
+else:
+  print("Positive")
