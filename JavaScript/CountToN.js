5d4e6c3c6be410172ca50c80fcea117680a5f5d1
Vincent Chriss <vchristianthen@gmail.com>
2026-07-31 18:31:58 +0700

    Update Challenge 15

---
+++ JavaScript/CountToN.js
@@ -0,0 +1,22 @@
+/*
+Challenge 15: Count to N
+
+Print all numbers from
+1 to N.
+
+Example:
+Input: 5
+
+Output:
+1
+2
+3
+4
+5
+*/
+
+let number = 6
+
+for (let i = 1; i < number; i++) {
+  console.log(i)
+}
---
+++ Python/CountToN.py
@@ -0,0 +1,19 @@
+# Challenge 15: Count to N
+
+# Print all numbers from
+# 1 to N.
+
+# Example:
+# Input: 5
+
+# Output:
+# 1
+# 2
+# 3
+# 4
+# 5
+
+number = int(input("Input a Number : "))
+
+for i in range(1, number):
+  print(i)
