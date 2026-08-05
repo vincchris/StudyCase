4546baad293b884d6dff2e7b474710b7350c2adf
Vincent Chriss <vchristianthen@gmail.com>
2026-08-05 21:16:34 +0700

    Update Challenge 21

---
+++ JavaScript/PrintAllNumber.js
@@ -0,0 +1,20 @@
+/*
+Challenge 21: Print All Elements
+
+Print every element
+inside an array.
+
+Example:
+[3, 7, 1]
+
+Output:
+3
+7
+1
+*/
+
+let number = [3, 7, 1]
+
+for (let i = 0; i < number.length; i++) {
+  console.log(number[i])
+}
---
+++ Python/PrintAllNumber.py
@@ -0,0 +1,18 @@
+# Challenge 21: Print All Elements
+
+# Print every element
+# inside an array.
+
+# Example:
+# [3, 7, 1]
+
+# Output:
+# 3
+# 7
+# 1
+
+number = [3, 7, 1]
+n = len(number)
+
+for i in range(n):
+  print(number[i])
